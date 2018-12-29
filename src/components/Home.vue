603faa6f01289efc979a05513c0a689ef6545a6c
tessai9 <lon1ago9@yahoo.co.jp>
2018-12-29 21:11:57 +0900

    default sources

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="home_list">
+        {{msg}}
+    </div>
+</template>
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'Home',
+  created: function () {
+    this.database = firebase.database()
+    this.vtuberList = this.database.ref('vtuber')
+  },
+  data () {
+    return {
+      msg: 'ここがホーム'
+    }
+  }
+}
+</script>
