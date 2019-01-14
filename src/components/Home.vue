838bf78359135d6ca0785ece007f3853a13a37d3
tessai9 <lon1ago9@yahoo.co.jp>
2019-01-14 22:40:20 +0900

    develop-1 HOMEの雛形を作成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,43 @@
 <template>
-    <div id="home_list">
-        {{msg}}
+  <div>
+    <div class='card' style='width: 18rem; display:inline-block;' v-for='item in vtubersList' :key='item.name'>
+      <img class='card-img-top' :src='item.image'>
+      <div class='card-body'>
+        <h5 class='card-title'>{{item.name}}</h5>
+        <p class='card-text'>.....</p>
+        <a :href='item.homepage' class='btn btn-primary'>Youtubeチャンネルへ</a>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import firebase from 'firebase'
 
 export default {
   name: 'Home',
-  created: function () {
-    this.database = firebase.database()
-    this.vtuberList = this.database.ref('vtuber')
-  },
   data () {
     return {
+      vtubersList: [],
       msg: 'ここがホーム'
     }
+  },
+  created: function () {
+    var database = firebase.database()
+    var getVtubersDatas = database.ref('/vtuber')
+    var vList = this.vtubersList
+    getVtubersDatas.once('value')
+      .then(function (snapshot) {
+        var n = 0
+        while (snapshot.child(n).val() != null) {
+          vList.push({
+            name: snapshot.child(n).child('name').val(),
+            channel: snapshot.child(n).child('youtube_channel').val(),
+            homepage: snapshot.child(n).child('homepage').val(),
+            image: require('../assets/' + snapshot.child(n).child('image_name').val())
+          })
+          n++
+        }
+      })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+console.log(process.env.firebase)
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyAimS8LuaTrUOO2HnQ7Q8jeWRzoCrcsG74',
+  authDomain: 'myapp-21f69.firebaseapp.com',
+  databaseURL: 'https://myapp-21f69.firebaseio.com',
+  projectId: 'myapp-21f69',
+  storageBucket: 'myapp-21f69.appspot.com',
+  messagingSenderId: '486585044717'
+}
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
