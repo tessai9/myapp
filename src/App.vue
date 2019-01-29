c8679e1edeff4df1fba83c94fa99e81ed0bfbed1
tessai9 <lon1ago9@yahoo.co.jp>
2019-01-30 00:28:02 +0900

    develop-6 ヘッダー部の雛形作成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <navbar style="margin-bottom:10px;"></navbar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
@@ -17,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
