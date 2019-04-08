cf9d358748fe3e4ab10565b30e81be6afd618ad2
tessai9 <lon1ago9@yahoo.co.jp>
2019-04-08 22:57:54 +0900

    develop-15 youtube API 連携機能実装

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,8 +7,8 @@
         <a :href='item.channel' target="_blank"><ion-icon name="logo-youtube" style="font-size: 50px;"></ion-icon></a>
         &nbsp;&nbsp;&nbsp;&nbsp;
         <a :href='item.homepage' target="_blank"><ion-icon name="home" style="font-size: 50px;"></ion-icon></a>
-        <br>
-        <img :src='item.newvideoimg'>
+        <div>最新動画</div>
+        <a :href='item.videourl' target="_blank"><img :src='item.newvideoimg'></a>
       </div>
     </div>  <!-- end of card -->
   </div>  <!-- end of container -->
@@ -16,51 +16,70 @@
 <script>
 import firebase from 'firebase'
 
+// firebase database
+async function getVtubersDatas () {
+  return firebase.database().ref('/vtuber').once('value')
+}
+
+// youtube api
+async function searchForYoutube (dataList) {
+  const youtubeApiKey = process.env.YOUTUBE_API_KEY
+  const youtubeApiUrl = 'https://www.googleapis.com/youtube/v3/search'
+  const reqUrl = '?part=snippet&maxResults=1&order=date&type=video&channelId=' + dataList.channelid + '&key=' + youtubeApiKey
+  return fetch(youtubeApiUrl + reqUrl)
+}
+
 export default {
   name: 'Home',
   data () {
     return {
       vtubersList: [],
-      getVtubersDatas: firebase.database().ref('/vtuber'),
       msg: 'ここがホーム'
     }
   },
   created: function () {
     const youtubeUrl = 'https://www.youtube.com/channel/'
+    const youtubeVideoUrl = 'https://www.youtube.com/watch?v='
     let self = this
-
-    self.getVtubersDatas.once('value')
+    getVtubersDatas()
       .then(function (snapshot) {
         let n = 0
+        let dataList = []
         while (snapshot.child(n).val() != null) {
-          self.vtubersList.push({
+          dataList.push({
             name: snapshot.child(n).child('name').val(),
             channel: youtubeUrl + snapshot.child(n).child('channel_id').val(),
             homepage: snapshot.child(n).child('homepage').val(),
             image: require('../assets/' + snapshot.child(n).child('image_name').val()),
             channelid: snapshot.child(n).child('channel_id').val()
           })
-          self.getNewVideoImg(self.vtubersList[n])
           n++
         }
+        return dataList
       })
-    for (let item in self.vtubersList) {
-      self.getNewVideoImg(item)
-    }
-    console.log(self.vtubersList)
-  },
-  methods: {
-    getNewVideoImg: function (vtuberData) {
-      let channelId = vtuberData.channelid
-      let youtubeApiKey = process.env.YOUTUBE_API_KEY
-      this.$axios.get('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' + channelId + '&maxResults=1&order=date&type=video&key=' + youtubeApiKey)
-        .then(response => {
-          vtuberData.newvideoimg = response.data.items[0].snippet.thumbnails.default.url
-        })
-        .catch(e => {
-          console.log(e)
-        })
-    }
+      .then(async function (dataList) {
+        let n = 0
+        while (dataList[n] != null) {
+          let imgUrl = ''
+          let videoId = ''
+          await searchForYoutube(dataList[n])
+            .then(function (response) {
+              return response.json()
+            })
+            .then(function (youtubeData) {
+              console.log(youtubeData.items[0].id)
+              imgUrl = youtubeData.items[0].snippet.thumbnails.default.url
+              videoId = youtubeData.items[0].id.videoId
+            })
+            .catch(e => function (e) {
+              console.log(e)
+            })
+          dataList[n].newvideoimg = imgUrl
+          dataList[n].videourl = youtubeVideoUrl + videoId
+          n++
+        }
+        self.vtubersList = dataList
+      })
   }
 }
 </script>
